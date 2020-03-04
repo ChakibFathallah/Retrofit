cdaa2aac93d1070d5c0287f2cbafac8ce5daf816
chakib <chakib.fathallah@esprit.tn>
2020-03-04 17:44:43 +0100

    Consuming ws examples with retrofit

--- app/src/main/java/com/example/retrofit/MainActivity.java
+++ app/src/main/java/com/example/retrofit/MainActivity.java
@@ -8,13 +8,22 @@ import com.example.retrofit.model.Comment;
 import com.example.retrofit.model.Post;
 import com.example.retrofit.utils.JsonPlaceHolderApi;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Callback;
+import retrofit2.HttpException;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -30,21 +39,59 @@ public class MainActivity extends AppCompatActivity {
 
         textViewResult = findViewById(R.id.text_view_result);
 
+        // for patch hardcoded it and set null value
+        Gson gson  = new GsonBuilder().serializeNulls().create();
+
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                // custom header for all request
+                /*
+                .addInterceptor(new Interceptor() {
+                    @NotNull
+                    @Override
+                    public okhttp3.Response intercept(@NotNull Chain chain) throws IOException {
+                        Request originalRequest = chain.request();
+
+                        Request newRequest  = originalRequest.newBuilder()
+                                .header("Interceptor-header","xyz")
+                                .build();
+
+                        return chain.proceed(newRequest);
+                    }
+                })
+
+                 */
+                .addInterceptor(loggingInterceptor)
+                .build();
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://jsonplaceholder.typicode.com/")
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
                 .build();
 
         jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
 
-        //getPosts();
+       // getPosts();
 
-        getComments();
+        //getComments();
+
+        //createPost();
+
+          updatePost();
+
+         //deletePost();
 
     }
 
+
+
     private void getPosts()
     {
+
+        // QueryMap
        /* Map<String,String> parameters = new HashMap<>();
         parameters.put("user_id","10");
         parameters.put("_sort","id");
@@ -125,5 +172,107 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+
+    private void createPost(){
+
+        //Post post = new Post(23,"new title","new text");
+        //Call<Post> call = jsonPlaceHolderApi.createPost(post);
+
+
+       // Call<Post> call = jsonPlaceHolderApi.createPost(23,"new title","new text");
+
+        Map<String,String> fields = new HashMap<>();
+        fields.put("userId","25");
+        fields.put("Title","New title");
+
+        Call<Post> call = jsonPlaceHolderApi.createPost(fields);
+
+
+        call.enqueue(new Callback<Post>() {
+            @Override
+            public void onResponse(Call<Post> call, Response<Post> response) {
+
+                if (!response.isSuccessful()){
+                    textViewResult.setText("code"+ response.code());
+                    return;
+                }
+
+                Post postResponse = response.body();
+
+                String content = "";
+                content += "Code" + response.code() + "\n" ;
+                content += "ID = " + postResponse.getId() + "\n" ;
+                content += "User ID = " + postResponse.getUserId() + "\n" ;
+                content += "Title = " + postResponse.getTitle() + "\n" ;
+                content += "text = " + postResponse.getText() + "\n\n" ;
+
+                textViewResult.setText(content);
+
+            }
+
+            @Override
+            public void onFailure(Call<Post> call, Throwable t) {
+
+                textViewResult.setText(t.getMessage());
+
+            }
+        });
+    }
+
+
+    private void updatePost(){
+        Post post = new Post(12,null,"new text");
+       // Call<Post>  call = jsonPlaceHolderApi.putPost(5,post);
+       // Call<Post>  call = jsonPlaceHolderApi.putPost("abc",5,post);
+
+        Map<String,String> headers = new HashMap<>();
+        headers.put("Map-Header1","def");
+        headers.put("Map-Header2","ghi");
+        Call<Post>  call = jsonPlaceHolderApi.patchPost(headers,5,post);
+
+
+        call.enqueue(new Callback<Post>() {
+            @Override
+            public void onResponse(Call<Post> call, Response<Post> response) {
+                if (!response.isSuccessful()){
+                    textViewResult.setText("code"+ response.code());
+                    return;
+                }
+
+                Post postResponse = response.body();
+
+                String content = "";
+                content += "Code" + response.code() + "\n" ;
+                content += "ID = " + postResponse.getId() + "\n" ;
+                content += "User ID = " + postResponse.getUserId() + "\n" ;
+                content += "Title = " + postResponse.getTitle() + "\n" ;
+                content += "text = " + postResponse.getText() + "\n\n" ;
+
+                textViewResult.setText(content);
+            }
+
+            @Override
+            public void onFailure(Call<Post> call, Throwable t) {
+
+            }
+        });
+    }
+
+
+    private void deletePost() {
+        Call<Void> call = jsonPlaceHolderApi.deletePost(5);
+
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                textViewResult.setText("Code" + response.code());
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                textViewResult.setText( t.getMessage());
+            }
+        });
+    }
 
 }
--- app/src/main/java/com/example/retrofit/utils/JsonPlaceHolderApi.java
+++ app/src/main/java/com/example/retrofit/utils/JsonPlaceHolderApi.java
@@ -1,7 +1,18 @@
 package com.example.retrofit.utils;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.Field;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.HeaderMap;
+import retrofit2.http.Headers;
+import retrofit2.http.PATCH;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 import retrofit2.http.QueryMap;
@@ -31,5 +42,39 @@ public interface JsonPlaceHolderApi {
 
     @GET
     Call<List<Comment>> getComments(@Url String url);
+
+    @POST("posts")
+    Call<Post> createPost(@Body Post post);
+
+    @FormUrlEncoded
+    @POST("posts")
+    Call<Post> createPost(
+            @Field("userId") int userId,
+            @Field("title")  String title,
+            @Field("body")  String body
+    );
+
+    @FormUrlEncoded
+    @POST("posts")
+    Call<Post> createPost(@FieldMap Map<String, String> fields);
+
+
+    @Headers({"Static-Headers1: 123", "Static-Header2: 456"})
+    @PUT("posts/{id}")
+    Call<Post> putPost(@Header("Dynamic-Header") String header,
+            @Path("id") int id,
+            @Body Post post
+    );
+
+    @PATCH("posts/{id}")
+    Call<Post> patchPost(@HeaderMap Map<String,String> headers,
+                         @Path("id") int id,
+                         @Body Post post);
+
+    @DELETE("posts/{id}")
+    Call<Void> deletePost(@Path("id") int id);
+
+
+
 
 }
